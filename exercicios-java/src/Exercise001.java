66196044de9907676881a238ea5ed38104daf7c3
Arian Melo <168552659+ArianMelo@users.noreply.github.com>
2024-05-17 10:46:08 -0300

    Exercise001
    Add Exercise001

---
+++ exercicios-java/src/Exercise001.java
@@ -0,0 +1,39 @@
+
+// 1. HOME
+// 2. INTRO
+// 3. GET STARTED (Java install)
+// 4. SYNTAX
+// 5. OUTPUT
+
+public class Exercise001 {
+    public static void main(String[] args) {
+        // 1. HOME
+        System.out.println("1. HOME");
+        System.out.println("Hello World\n");
+
+        // 2. INTRO
+        System.out.println("2. INTRO");
+        System.out.println("\"public static void main\" é um método principal.");
+
+        // 3. GET STARTED
+        System.out.println("3. GET STARTED");
+        System.out.println("Use o W3C para instalar o Java version,o JDK e a versão.\n");
+
+        // 4. SYNTAX
+        System.out.println("4. SYNTAX");
+        System.out.println("\"{}\" é o bloco onde é inserido o código do método.");
+        System.out.println("\"System\" é uma classe integrada do java(reservada).");
+        System.out.println("\"out\" é uma abreviação de saída.\n");
+
+        // 5. OUTPUT
+        System.out.println("5. OUTPUT");
+        System.out.println("\"println\"Imprime o conteúdo em uma linha apenas");
+        System.out.print("1.\"print\"Imprime o conteúdo na mesma linha.");
+        System.out.println("2.\"print\"Imprime o conteúdo na mesma linha");
+        System.out.println("Números são exibidos fora da aspas: "+ 334);
+        System.out.println(1000);
+        System.out.println(5000);
+        System.out.println("Cálculo matemático, soma de 4+5: "+ (4+5));
+        System.out.println("Cálculo matemático, multiplicação de 2 * 5: "+ (2 * 5));
+    }
+}
