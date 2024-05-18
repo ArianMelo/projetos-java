a3957f73870638e131c366e5e4e0eb7dc57f3cb8
Arian Melo <168552659+ArianMelo@users.noreply.github.com>
2024-05-18 08:07:53 -0300

    Exercicio003

--- exercicios-java/src/Exercise002.java
+++ exercicios-java/src/Exercise002.java
@@ -11,11 +11,12 @@ public class Exercise002 {
         // 6. COMMENTS
         System.out.println("6. COMMENTS");
         System.out.println("\"//\" Comentários em linha.");
-        System.out.println("\"/*...*/\" Comentários em bloco.\n");
+        System.out.println("\"/*...*/\" Comentários em bloco.");
+        System.out.println("-----------------------------------");
 
         // 7.1 VARIABLES
-        System.out.println("7. VARIABLES");
-        System.out.println("Tipos de primitivos: String, int, float, char e boolean.\n");
+        System.out.println("7.1 VARIABLES");
+        System.out.println("Tipos de primitivos: int, float, char e boolean.\n");
         // Tipos de variáveis: String, int, float, double, char e boolean.
         System.out.println("String: inserção de texto concatenado dentro de aspas duplas.");
         System.out.println("int: valores inteiros como +123 e -123.");
@@ -39,11 +40,13 @@ public class Exercise002 {
         System.out.println("Esse valor é imutável, usado para valores estáticos como PI = 3,14..."+finalNum);
         // Variável em cadeia de valores: "tipo var1 = val1, var2 = val2, var3 = val3
         int valA = 5, valB = 6, valC = 50;
-        System.out.println(valA * valB * valC + "\n");
+        System.out.println(valA * valB * valC);
+        System.out.println("-----------------------------------");
 
         // 7.2 PRINT VARIABLES
 
         // Use o "+" para concatenar caracteres e variáveis.
+        System.out.println("7.2 PRINT VARIABLES");
         String name2 = "Luís";
         String lastName2 = " Roberto";
         String fullName2 = name2 + lastName2;
@@ -51,9 +54,145 @@ public class Exercise002 {
         int numVar1 = 20;
         int numVar2 = 40;
         System.out.println(numVar1 + numVar2);
+        System.out.println("-----------------------------------");
 
         // 7.3 MULTIPLAS VARIÁVEIS
+        System.out.println("7.3 MULTIPLAS VARIÁVEIS");
+        // Pode declarar mais de variável do mesmo tipo
+        int numVar3 = 10, numVar4 = 15, numVar5 = 25;
+        System.out.println("Variáveis adicionadas em linha: " + numVar3 + numVar4 + numVar5);
+        System.out.println("-----------------------------------");
 
+        // 7.4 IDENTIFIERS
+        System.out.println("7.3 MULTIPLAS VARIÁVEIS");
+        // As variáveis devem ter nomes exclusivos, chamados identificadores:
+        // - Pode conter linhas, sublinhados, digitos e cifrões;
+        // - Devem começar com uma letra;
+        // - Devem começar com letra minúscula e não podem espaços em branco;
+        // - Podem começar com '$';
+        // - Maiúsculas e minúsculas são diferenciadas;
+        // - Não podem ser usadas palavras reservadas;
+        System.out.println("-----------------------------------");
 
+        // 7.5 EXAMPLES
+        System.out.println("7.5 EXAMPLES");
+        System.out.println("Exercício de variável: Área do Retângulo");
+        int ladoDoRetangulo = 4;
+        int baseDoRetangulo = 5;
+        final int areaDoRetangulo = ladoDoRetangulo * baseDoRetangulo;
+        System.out.println("Lado: "+ ladoDoRetangulo);
+        System.out.println("Base: "+ baseDoRetangulo);
+        System.out.println("Área do Retângulo: "+ areaDoRetangulo);
+        System.out.println("-----------------------------------");
+
+        // 8.1 DATA TYPES
+        // Dados primitivas: byte, short, int, long, float, double, boolean e char;
+        // Dados não primitivos: String, Arrays, Classes;
+        System.out.println("8.1 DATA TYPES");
+        System.out.println("Dados primitivas: byte, short, int, long, float, double, boolean e char");
+        System.out.println("Dados não primitivos: String, Arrays e Classes.\n");
+        System.out.println("byte: 1 byte (-128 até 127");
+        System.out.println("short: 2 bytes (-32,768 até 32,767");
+        System.out.println("int: 4 bytes (-2,147,483,648 até 2,147,483,647");
+        System.out.println("long: 8 bytes (-9.10^18 até 9.10^180)");
+        System.out.println("float: 4 bytes (6 a 7 números decimais. Ex.: 1.1234567F");
+        System.out.println("double: 8 bytes (até 15 números decimais. Ex.: 1.123456789123456)");
+        System.out.println("boolean: 1 bit (true = 1 e false = 0)");
+        System.out.println("char: 2 bytes (apresenta apenas um caracteres entre aspas simples. Ex.: 'A'");
+        System.out.println("-----------------------------------");
+
+        // 8.2 DATA TYPES: NUMBERS
+        System.out.println("8.2 DATA TYPES: NUMBERS");
+        byte numByte1 = 100;
+        short numShort1 = 5000;
+        int numInt1 = 1000000;
+        long numLong1 = 100_000_000_000L;
+        float numFloat1 = 5.345f;
+        double numDouble1 = 19.12345d;
+        float numCience1 = 125e3f;
+        double numCience2 = 12E4d;
+        System.out.println("Byte: " + numByte1);
+        System.out.println("Short: " + numShort1);
+        System.out.println("Int: " + numInt1);
+        System.out.println("Long: " + numLong1);
+        System.out.println("Float: " + numFloat1);
+        System.out.println("Double: " + numDouble1);
+        System.out.println("Número científico usando float: " + numCience1);
+        System.out.println("Número científico usando double: " + numCience2);
+        System.out.println("-----------------------------------");
+
+        // 8.3 DATA TYPES: BOOLEAN
+        System.out.println("8.3 DATA TYPES: BOOLEAN");
+        System.out.println("O boolean pode ser: (SIM ou NÃO), (LIGADO ou DESLIGADO) e (VERDADEIRO e FALSO)");
+        System.out.println("-----------------------------------");
+
+        // 8.4 DATA TYPES: CHARACTERS
+        System.out.println("8.4 DATA TYPES: CHARACTERS");
+        char meuChar1 = 'A';
+        System.out.println(meuChar1);
+        // Alguns números podem ser representados como caracteres: A = 65, B = 66, C = 67;
+        System.out.println("A = 65, B = 66, C = 67");
+        System.out.println("String: são cadeias de caracteres unidas para formar texto.");
+        System.out.println("-----------------------------------");
+
+        // 8.5 DATA TYPES: EXERCISE
+        System.out.println("8.5 DATA TYPES: EXERCISE");
+        int numItems1 = 50;
+        float costItems1 = 9.99f;
+        float totalCostItems1 = numItems1 * costItems1;
+        char currency1 = '$';
+        System.out.println("Números de itens: "+ numItems1);
+        System.out.println("Custo por item: "+ costItems1 + currency1);
+        System.out.println("Custo total: "+ totalCostItems1 + currency1);
+        System.out.println("-----------------------------------");
+
+        // 8.6 DATA TYPES: NON-PRIMITIVE DATA TYPES
+        System.out.println("8.6 DATA TYPES: NON-PRIMITIVE DATA TYPES");
+        // Tipos de dados não primitivos são chamados de referência porque se referem a objetos.
+        // TIPOS PRIMITIVOS são definidos pelo JAVA;
+        // TIPOS PRIMITIVOS começa com letra minúscula;
+        // TIPOS NÃO PRIMITIVOS são definidos pelo programador;
+        // TIPOS NÃO PRIMITIVOS podem charmar métodos e realizar operações.
+        // TIPOS NÃO PRIMITIVOS podem ter valores "null";
+        // TIPOS NÃO PRIMITIVOS podem começar com letra maiúsculas;
+        // TIPOS NÃO PRIMITIVOS são: String, Arrays, Classes, Interface e outros...
+        System.out.println("-----------------------------------");
+
+        // 9.0 TYPE CASTING
+
+        System.out.println("9.0 TYPE CASTING");
+        System.out.println("Widening Casting - automático, conversão de tipo menor para maior.");
+        System.out.println("byte -> short -> char -> int -> long -> float -> double \n");
+        System.out.println("Narrowing Casting - manual, conversão de tipo maior para menos.");
+        System.out.println("double -> float -> long -> int -> char -> short -> byte \n");
+        int numAutoInt1 = 10;
+        double numAutoDouble1 = numAutoInt1; // Casting Automático: int(menor) para double(maior) - AUTOMÁTICO.
+        System.out.println("Valor em int, antes da conversão: " + numAutoInt1);
+        System.out.println("Valor em double, depois da conversão: " + numAutoDouble1);
+        double numManualDouble1 = 54.99d;
+        int numManualInt1 = (int) numManualDouble1; // Casting Manual: double(maior) para int(menor) - FORÇADO.
+        System.out.println(numManualDouble1);
+        System.out.println(numManualInt1);
+        System.out.println("-----------------------------------");
+
+        // 9.1 TYPE CASTING: EXERCISE
+        System.out.println("9.1 TYPE CASTING: EXERCISE");
+        // Set the maximum possible score in the game to 500
+        int maxScore = 500;
+        // The actual score of the user
+        int userScore = 435;
+        /* Calculate the percantage of the user's score in relation to the maximum available score.
+Convert userScore to float to make sure that the division is accurate */
+        float percentScore = (float) userScore / maxScore * 100.0f;
+        System.out.println(percentScore + "%");
+        System.out.println("-----------------------------------");
+
+        // 10. OPERATORS
+        System.out.println("10. OPERATORS");
+        //Operadores aritméticos - Matemáticos: (+, -, *, /, %, ++, --)
+        //Operadores de atribuição - Atribuis valores: ( = , +=, -=, *=, /=, %=, &=, |=, ^=, >>=, <<=)
+        //Operadores de comparação - Comparar valores booleanos: ( ==, !=, >, <, >=, <=)
+        //Operadores lógicos - Comparar valores booleanos: (&& - and , || - ou, ! - not)
+        //Operadores bit a bit: (permitem a avaliação e modificação de bits específicos em um tipo inteiro)
     }
 }
---
+++ exercicios-java/src/Exercise003.java
@@ -0,0 +1,164 @@
+
+    // 11. STRINGS
+    // 12. MATH
+    // 13. BOOLEANS
+    // 14. IF...ELSE
+    // 15. SWITCH
+
+public class Exercise003 {
+    public static void main(String[] args) {
+
+        // 11.1 STRINGS
+        // Métodos de String: length(), toUpperCase(), toLowerCase().
+        System.out.println("11.1 STRINGS");
+        String txtStrings = "abcdefghijklmnopqrstuvwxyz";
+        System.out.println("Tamanho da String exibido pelo método \"length\": "+ txtStrings.length());
+        System.out.println("String exibido em letra maiúscula pelo método \"toUpperCase()\": "+ txtStrings.toUpperCase());
+        System.out.println("String exibido em letra minúscula pelo método \"toLowerCase()\": "+ txtStrings.toLowerCase());
+        System.out.println("String retorna o index(posição) pelo método \"indexOf()\": "+ txtStrings.indexOf("a"));
+        System.out.println(txtStrings.indexOf("ab"));// 0 é a primeira posição
+        System.out.println(txtStrings.indexOf("efg"));
+        System.out.println(txtStrings.indexOf("xyz"));// Conta a partir do primeiro caracetere.
+        System.out.println("-----------------------------------");
+
+        // 11.2 STRINGS: CONCATENATION
+        System.out.println("11.2 STRINGS: CONCATENATION");
+        // O sinal de + é usado para concatenar strings.
+        String firstName1 = "John";
+        String lastName1 = "Willians";
+        System.out.println(firstName1 + " " + lastName1);
+        // concat(): pode ser usado para concatenar duas strings.
+        firstName1 = "John ";
+        System.out.println(firstName1.concat(lastName1));
+        System.out.println("-----------------------------------");
+
+        // 11.3 STRINGS: STRING AND NUMBERS
+        System.out.println("11.3 STRINGS: STRING AND NUMBERS");
+        int intVal01 = 10;
+        int intVal02 = 20;
+        int intTotal01 = intVal01 + intVal02; // Resultado numérico.
+        System.out.println("Concatenação de variáveis matemática gera um resultado: "+ intTotal01);
+        String stgVal01 = "10";
+        String stgVal02 = "20";
+        String stgResult01 = stgVal01 + stgVal02; // Resultado em texto.
+        System.out.println("Concatenação de Strings gera um texto unido: "+ stgResult01);
+        System.out.println("-----------------------------------");
+
+        // 11.4 STRINGS: SPECIAL CHARACTERS
+        System.out.println("11.4 STRINGS: SPECIAL CHARACTERS");
+        // Caracteres especiais: \' -> ', \" -> ", \\ -> \;
+        System.out.println("Texto com barra: \'aspas simples\', \"aspas duplas\", \\repositorio");
+        System.out.println("Texto com barra: pula linha\n");
+        System.out.println("Texto com barra: \rquebra linha");
+        System.out.println("Texto com barra:\ttab");
+        System.out.println("Texto com barra: apagax\b uma caractere anterior");
+        System.out.println("Texto com barra: \fforma");
+        // \n -> pula uma linha, \r -> quebra linha, \t -> TAB, \b -> Backspace, \f ->
+        System.out.println("-----------------------------------");
+
+        // 12. MATH
+        System.out.println("12. MATH");
+        // Método Math.max(x,y): Maior valor entre os valores dentro dos parênteses;
+        System.out.println("Método: Math.max(5,10): "+ Math.max(5,10));
+        // Método Math.min(x,y): Menor valor entre os valores dentro dos parênteses;
+        System.out.println("Método: Math.min(5,10): "+ Math.min(5,10));
+        // Método Math.sqrt(x): Retorna raiz quadrada do valor;
+        System.out.println("Método: Math.sqrt(2): "+ Math.sqrt(2));
+        // Método Math.abs(x): Retorna valor absoluto(módulo), -5 = |5|;
+        System.out.println("Método: Math.abs(-2): "+ Math.abs(-2));
+        // Método Math.random(): Retorna valor aleatório entre 0-1, ele pode sem multiplicado por outro valor;
+        System.out.println("Método: Math.random(): "+ Math.random());
+        System.out.println("Método: Math.random(): "+ Math.random() * 101); // 0 até 100
+        System.out.println("-----------------------------------");
+
+        // 13. BOOLEANS
+        System.out.println("13. BOOLEANS");
+        // BOOLEANOS: (SIM/NÃO), (LIGADO/DESLIGADO), (VERDADEIRO/FALSO);
+        boolean optionT = true;
+        boolean optionF = false;
+        System.out.println(optionT);
+        System.out.println(optionF);
+        // Operador de comparação
+        int intCom1 = 10;
+        int intCom2 = 20;
+        System.out.println("10 > 20: "+ (intCom1 > intCom2));
+        System.out.println("10 < 20: "+ (intCom1 < intCom2));
+        System.out.println("-----------------------------------");
+
+        // 13.1 BOOLEANS: IF...ELSE
+        System.out.println("13.1 BOOLEANS: IF...ELSE");
+        int intVal1 = 1;
+        if (intVal1 < 5) {
+            System.out.println("O valor é menor que 5.");
+        } else if (intVal1 <= 7) {
+            System.out.println("O valor está entre 5 e 7");
+        } else {
+            System.out.println("O valor é maior que 7");
+        }
+        System.out.println("-----------------------------------");
+
+        // 14. IF...ELSE
+        System.out.println("14. IF...ELSE");
+        /* Condições lógicas usuais da matemática:
+        * Menor que: a < b;
+        * Menor ou igual a: a <= b;
+        * Maior que: a > b;
+        * Maior ou igual a: a >= b
+        * Igual a: a == b;
+        * Diferente de: a! = b;
+         */
+
+        /* Instruções condicionais:
+        * if: especificar o bloco de código a ser executado, se a condição for verdadeira;
+        * else: especificar um bloco de código a ser executado, se a condição for falsa;
+        * else if: para especificar uma nova condição a ser testada, se a primeira condição for falsa;
+        * switch: para especificar muitos bloco alternativos de código a serem executados.
+        */
+        intVal1 = 20;
+        int intVal2 = 40;
+
+        if (intVal1 >= 20) {
+            System.out.println("Verdadeiro");
+        } else {
+            System.out.println("Falso");
+        }
+
+        if (intVal1 >= 40) {
+            System.out.println("Valor 1 é maior que 40");
+        } else if (intVal2 <= 30) { // else...if é operador ternário -> consistem em três operandos.
+            System.out.println("Valor 2 é menor que 30");
+        } else {
+            System.out.println("Valor 1 é menor que 40 e Valor 2 é maior que 30");
+        }
+        // Instrução em uma única linha:
+        // variable = (condição) ? espressaoVerdadeira : espressaoFalse;
+        String result1 = (intVal1 >= 10) ? "Valor 1 é maior que 40." : "Valor 1 é menor que 40 e Valor 2 é maior que 30";
+        System.out.println(result1);
+
+        // 14. IF...ELSE: EXERCISE
+
+        int doorCode1 = 1234;
+        int doorCode2 = 1357;
+
+        if (doorCode1 == 1234) {
+            System.out.println("Senha correta. A porta pode ser aberta.");
+        } else {
+            System.out.println("Senha incorreta. Tente novamente");
+        }
+
+        String doorLock = (doorCode2 == 1356) ? "Senha correta, pode abrir" : "Senha incorreta, tente novamente.";
+        System.out.println(doorLock);
+
+        System.out.println("-----------------------------------");
+
+        // 15. SWITCH
+        System.out.println("15. SWITCH");
+
+
+
+
+
+
+
+    }
+}
